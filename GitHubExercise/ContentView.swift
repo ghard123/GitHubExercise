a8f7c84ec099ca8b07c0f324488cb3d5f6bf5caf
Greg <ghard123@gmail.com>
2024-09-06 14:41:01 +1000

    Test commit

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "1.circle")
+            Image(systemName: "circle")
                 .resizable()
                 .scaledToFit()
             Text("Version. 1.0")
