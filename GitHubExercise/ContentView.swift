6939a2326c5aca8293fac19b91a2d3fbdfbf2ec5
Greg <ghard123@gmail.com>
2024-09-06 13:57:30 +1000

    Initial Commit

---
+++ GitHubExercise/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  GitHubExercise
+//
+//  Created by Greg on 6/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+            Text("Version. 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
+        }
+        .padding()
+        .foregroundStyle(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Greg on 6/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
